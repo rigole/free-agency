69caea9dfe1c5136c9b2b2acd3b62f55ce4ee4d3
rigole <foplacide@gmail.com>
2022-01-03 11:02:04 +0100

    Setting the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,55 @@
 import logo from "../../logo.svg";
 import "../../App.css";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+import { StyledLink } from "../../utils/Atoms";
+
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const HomeContainer = styled.div`
+  margin: 30px;
+  background-color: ${colors.backgroundLight};
+  padding: 60px 90px;
+  display: flex;
+  flex-direction: row;
+  max-width: 1200px;
+
+`
+
+const LeftCol = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  ${StyledLink}{
+    max-width: 250px;
+  }
+`
+
+const StyledTitle = styled.h2`
+  padding-bottom: 30px;
+  max-width: 280px;
+  line-height: 50px;
+`
 
 function Home() {
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <HomeWrapper>
+     <HomeContainer>
+       <LeftCol>
+         <StyledTitle>
+           Reperez vos besoins on s'occupe du reste avec les meilleurs talents
+         </StyledTitle>
+         <StyledLink to="/survey/1" $isFullLink>
+           Faire le test
+         </StyledLink>
+       </LeftCol>
+     </HomeContainer>
+   </HomeWrapper>
   );
 }
 
---
+++ src/utils/Atoms.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+import colors from "./style/colors";
+import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+    padding: 10px 15px;
+    color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  text-align: center;
+  ${(props) =>
+    props.$isFullLink && `color:white; border-radius:30px; background-color: ${colors.primary}`}
+
+
+`
