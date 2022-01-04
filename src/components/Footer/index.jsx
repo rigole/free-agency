e1ae019740a14d24960b376c5e39cc4e771e2cf8
rigole <foplacide@gmail.com>
2022-01-04 20:39:47 +0100

    creating Footer components for Dark mode and its style

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 60px;
+`
+const NightModeButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: ${colors.secondary};
+
+`
+
+
+function Footer() {
+
+    return()
+}
+
+export default Footer
