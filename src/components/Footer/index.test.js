c46a587354b0627bf32a2a5b043806662e91be24
rigole <foplacide@gmail.com>
2022-01-10 09:43:00 +0100

    test on the Footer component 2

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,13 +1,30 @@
 import Footer from "./index";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import {ThemeProvider} from "../../utils/context";
 
-describe('Footer', () => {
-    test('Should render without crash', async () => {
-        render(
-            <ThemeProvider>
-                <Footer/>
-            </ThemeProvider>
-        )
+    describe('Footer', () =>{
+
+        test('should render without crashing', async () => {
+            render(
+                <ThemeProvider>
+                    <Footer/>
+                </ThemeProvider>
+            )
+        })
+
+
+        test('Change Theme', async () => {
+            render(
+                <ThemeProvider>
+                    <Footer/>
+                </ThemeProvider>
+            )
+            const nightModeButton = screen.getByRole('button')
+            expect(nightModeButton.textContent).toBe('Changer de mode : ☀️')
+            fireEvent.click(nightModeButton)
+            expect(nightModeButton.textContent).toBe('Changer de mode : 🌙')
+
+        })
+
     })
-})
+
